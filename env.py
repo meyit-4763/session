d65357511e64057739ac1776678e214e65995e00
meyit-4763 <sumeyyeomeryesilyurt@gmail.com>
2024-12-22 17:22:16 +0300

    Update env.py

--- env.py
+++ env.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = os.getenv("API_ID", "").strip()
-API_HASH = os.getenv("API_HASH", "").strip()
-BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
-DATABASE_URL = os.getenv("DATABASE_URL", "").strip() # Not a necessary variable anymore but you can add to get stats
-MUST_JOIN = os.getenv("MUST_JOIN", "")
+API_ID = os.getenv("API_ID", "22768571").strip()
+API_HASH = os.getenv("API_HASH", "7d92204d9b502be216843739f70ded0e").strip()
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7604555634:AAGWMfCKmcumls1Pr0y1QyqTDrUPKcqyHns").strip()
+DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://meyitzadee:Mytbot47@mytbot.a6b81.mongodb.net/?retryWrites=true&w=majority&appName=MytBot").strip() # Not a necessary variable anymore but you can add to get stats
+MUST_JOIN = os.getenv("MUST_JOIN", "MytSohbet")
 
 if not API_ID:
     raise SystemExit("No API_ID found. Exiting...")
